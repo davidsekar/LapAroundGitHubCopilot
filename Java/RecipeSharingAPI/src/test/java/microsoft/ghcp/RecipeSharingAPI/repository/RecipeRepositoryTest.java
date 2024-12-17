79218a81aa0643f9731ea9e923b6bd992a06eee4
AntonioMisic77 <antonio.misic77@outlook.com>
2024-12-17 15:34:19 +0100

    Unit testing java done.

---
+++ Java/RecipeSharingAPI/src/test/java/microsoft/ghcp/RecipeSharingAPI/repository/RecipeRepositoryTest.java
@@ -0,0 +1,19 @@
+package microsoft.ghcp.RecipeSharingAPI.repository;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+public class RecipeRepositoryTest {
+
+    @Test
+    public void Dummy_Test() {
+        // ArrangeÍ
+        int a = 2;
+        int b = 10;
+        int c = a + b;
+
+        // Assert
+        assertThat(c).isEqualTo(12);
+
+    }
+}
+
