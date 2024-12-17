4218c2fa342aa267c7450b832f90000ec09692f1
AntonioMisic77 <antonio.misic77@outlook.com>
2024-12-17 15:21:38 +0100

    unit testing .net implemented

---
+++ C#/RecipeSharingAPI.Test/Test.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using Microsoft.EntityFrameworkCore;
+using RecipeSharingAPI.Models;
+using RecipeSharingAPI.Services;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RecipeSharingAPI.Tests
+{
+    [TestFixture]
+    public class RecipeServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            
+        }
+
+        [Test]
+        public void Test_Example()
+        {
+            
+        }
+    }
+}
