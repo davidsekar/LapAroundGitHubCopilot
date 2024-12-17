c01330a4ab8766979ce00499ee81c67b5869a655
AntonioMisic77 <antonio.misic77@outlook.com>
2024-12-17 15:44:36 +0100

    unit testing mocha js done

---
+++ Javascript/RecipeSharingAPI/test/db.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const Database = require('../src/db');
+
+describe('Test', function () {
+
+    beforeEach(async function () {
+    });
+
+    afterEach(async function () {
+    });
+
+    describe('Test', function () {
+        it('Test', async function () {
+        });
+    });
+});
